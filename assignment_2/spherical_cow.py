302098bc3740d7909babc0b917cee979d5a8fd98
ninoco <neoneill@syr.edu>
2025-09-02 13:03:21 -0400

    intialziing the functions needed for the code

---
+++ assignment_2/spherical_cow.py
@@ -0,0 +1,27 @@
+# setting initial conditions
+m = 1000 # kg
+time = 0
+ts = 20
+x = 0
+y = 0
+v = 0
+
+def tot_force_vect(curr_x, curr_y, velocity):
+    curr_x = x
+    curr_y = y
+    velocity = v
+    return force 
+
+def update_pos(init_pos, velocity, force, ts):
+    init_x = x
+    init_y = y
+    velocity = v
+    
+    return x,y
+
+def calc_forces():
+    pot = 
+    kin = 
+    tot = 
+    return pot, kin, tot
+
