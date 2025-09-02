cc66a45b7fa9e060184a3dda64a653b7b689178a
ninoco <neoneill@syr.edu>
2025-09-02 13:53:01 -0400

    dividing work

--- assignment_2/spherical_cow.py
+++ assignment_2/spherical_cow.py
@@ -1,31 +1,32 @@
+import numpy as np
 # setting initial conditions
 m = 1000 # kg
 time = 0
-ts = 20
-x = 0
-y = 100 # height of cow
-v = 0 # initial velocity
+ts = 0.1
+pos = (0, 100) # inital position
+v = (0, 0) # initial velocity
 g = 9.8 # m/s
-
 const = 3 # wind resistnace constant
 
-def tot_force_vect(curr_x, curr_y, velocity):
-    curr_x = x
-    curr_y = y
-    velocity = v
+# sadman
+def tot_force_vect(position, velocity):
+    position = pos
+    height = position(1)
+    velocity = np.sqrt(2 * g * height)
 
     wind = const * velocity**2
     grav = - m * g
     force = wind + grav
-    return force 
+    a = 1
+    return force
 
-def update_pos(init_pos, velocity, force, ts):
-    init_x = x
-    init_y = y
-    velocity = v
+# sadman
+def update_pos(position, velocity, force):
+    position += 
     
-    return x,y
+    return position, velocity
 
+# nico
 def calc_forces():
     h = y
     pot = m * g * h
@@ -33,3 +34,9 @@ def calc_forces():
     tot = pot + kin
     return pot, kin, tot
 
+# nico
+for i in smth:
+    time += ts
+    f = tot_force_vect(pos, v)
+    pos, vel = update_pos(pos, v, f)
+    if pos(0) & pos(1) =
