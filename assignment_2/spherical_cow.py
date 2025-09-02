85af5d91c6a2a2089fa752f28d6163080c56675a
ninoco <neoneill@syr.edu>
2025-09-02 13:10:00 -0400

    progress setting up total force vector

--- assignment_2/spherical_cow.py
+++ assignment_2/spherical_cow.py
@@ -3,13 +3,20 @@ m = 1000 # kg
 time = 0
 ts = 20
 x = 0
-y = 0
-v = 0
+y = 100 # height of cow
+v = 0 # initial velocity
+g = 9.8 # m/s
+
+const = 3 # wind resistnace constant
 
 def tot_force_vect(curr_x, curr_y, velocity):
     curr_x = x
     curr_y = y
     velocity = v
+
+    wind = const * velocity**2
+    grav = - m * g
+    force = wind + grav
     return force 
 
 def update_pos(init_pos, velocity, force, ts):
@@ -20,8 +27,9 @@ def update_pos(init_pos, velocity, force, ts):
     return x,y
 
 def calc_forces():
-    pot = 
-    kin = 
-    tot = 
+    h = y
+    pot = m * g * h
+    kin = m * v**2 / 2
+    tot = pot + kin
     return pot, kin, tot
 
