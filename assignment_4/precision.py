ea074b5b6754d9a41831deb5ad59e1d02b82ef58
ninoco <neoneill@syr.edu>
2025-09-23 13:13:11 -0400

    adding operations

--- assignment_4/precision.py
+++ assignment_4/precision.py
@@ -5,6 +5,14 @@ class Precision():
         #return num
     def printing(self):
         print(self.num)
+    def __add__(self, a, b):
+        return self.a + self.b
+    def __sub__(self, a, b):
+        return self.a - self.b
+    def __mul__(self, a, b):
+        return self.a * self.b
+    def __div__(self, a, b):
+        return self.a / self.b
 # %%
 a = Precision(7.1)
 a.printing()
