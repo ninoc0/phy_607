53710b7403ed2041f198e70c4a19006bd62206ca
ninoco <neoneill@syr.edu>
2025-09-23 18:47:02 -0400

    pushing intialization of project 1

---
+++ project_1/euler_explicit.py
@@ -0,0 +1,13 @@
+"""
+Explicit Euler method is a first order numerical integration method that 
+approximates solutions to ODEs using a Taylor expansion.
+
+"""
+
+def euler_exp():
+    """Estimates the solution to an ODE using Explicit Euler method."""
+    # import initial conditions
+
+    # find the derivative
+
+    # find x_(n+1) = x(n) + ts * dx(n)/dt
---
+++ project_1/euler_sympletic.py
@@ -0,0 +1,13 @@
+"""
+Sympletic Euler method is a first order numerical integration method that 
+approximates solutions to ODEs using two step Euler updating.
+
+"""
+
+def euler_sym():
+    """Estimates the solution to an ODE using Sympletic Euler method."""
+    # import initial conditions
+
+    # find the derivative
+
+    # find x_(n+1) = x(n) + ts * dx(n)/dt
